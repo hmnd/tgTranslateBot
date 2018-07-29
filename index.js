fdf4eed8e0c0f9e198ece6e0ba3abc90ed1d5658
David Roizenman <david@hmnd.io>
2018-07-28 22:55:05 -0400

    Refactor to use async/await.

--- index.js
+++ index.js
@@ -13,41 +13,32 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
   const textToUrl = text =>
     "https://translate.google.com/#auto/en/" + encodeURIComponent(text);
 
-  bot.on("text", ctx => {
-    browser.newPage().then(page =>
-      page
-        .goto(textToUrl(ctx.message.text), { waitUntil: "networkidle0" })
-        .then(() =>
-          page
-            .waitForFunction(
-              () =>
-                document.querySelector("#result_box") !== null &&
-                document
-                  .querySelector("#result_box")
-                  .innerText.indexOf("......") === -1
-            )
-            .then(() =>
-              page
-                .evaluate(() => {
-                  const lang = document.querySelector(
-                    "#gt-sl-sugg > div > div:last-child"
-                  );
-                  const langFormatted = lang.innerText.slice(0, lang.innerText.indexOf(" -"));
-                  return langFormatted === "English"
-                    ? ""
-                    : document.querySelector("#result_box").innerText +
-                      "\n\nDetected language: " +
-                      langFormatted;
-                })
-                .then(text =>
-                  ctx
-                    .replyWithMarkdown(text, Extra.inReplyTo(ctx.message.message_id))
-                    .then(page.close())
-                    .catch(() => {})
-                )
-            )
-        )
+  bot.on("text", async ctx => {
+    const page = await browser.newPage();
+    await page.goto(textToUrl(ctx.message.text), { waitUntil: "networkidle0" });
+    await page.waitForFunction(
+      () =>
+        document.querySelector("#result_box") !== null &&
+        document.querySelector("#result_box").innerText.indexOf("......") === -1
     );
+    const text = await page.evaluate(() => {
+      const lang = document.querySelector("#gt-sl-sugg > div > div:last-child");
+      const langFormatted = lang.innerText.slice(
+        0,
+        lang.innerText.indexOf(" -")
+      );
+      return langFormatted === "English"
+        ? ""
+        : document.querySelector("#result_box").innerText +
+            "\n\nDetected language: " +
+            langFormatted;
+    });
+    if (text.length > 0) {
+      ctx
+        .replyWithMarkdown(text, Extra.inReplyTo(ctx.message.message_id))
+        .then(page.close())
+        .catch(() => {});
+    }
   });
 
   bot.startPolling();
