aab0832f086f67d9805537d5a0b547d7c919ceab
David Roizenman <david@hmnd.io>
2018-11-29 03:47:41 -0500

    Update to work with Material 2 Google Translate

--- index.js
+++ index.js
@@ -22,14 +22,17 @@ const logger = winston.createLogger({
 });
 
 const textToUrl = (text, lang = "en") =>
-  `https://translate.google.com/#auto/${lang}/${encodeURIComponent(text)}`;
+  `https://translate.google.com/#view=home&op=translate&sl=auto&tl=${lang}&text=${encodeURIComponent(text)}`;
 
 (async () => {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    headless: true
+    headless: false
   });
   const translateText = async inText => {
+    if (inText.trim().length > 0) {
+      return null;
+    }
     const page = await browser.newPage();
     await page
       .goto(textToUrl(inText), {
@@ -39,23 +42,23 @@ const textToUrl = (text, lang = "en") =>
       .catch(e => logger.error(e).then(translateText(inText)));
     await page.waitForFunction(
       () =>
-        document.querySelector("#result_box") !== null &&
-        document.querySelector("#result_box").innerText.indexOf("......") === -1
+      document.querySelector("span.tlid-translation") !== null &&
+      document.querySelector("span.tlid-translation").innerText.indexOf("......") === -1
     );
     const translation = await page.evaluate(() => {
       const isNotSourceLang = (sourceLang, destLang) =>
         !sourceLang.includes(destLang);
       const extractLang = lang => lang.slice(0, lang.indexOf(" -")) || null;
-      const lang = document.querySelector("#gt-sl-sugg > div > div:last-child")
+      const lang = document.querySelector("div.sl-sugg-button-container > div:first-child")
         .innerText;
       if (lang === "Detect language") {
         return null;
       }
       const langName = extractLang(lang);
-      if (langName && isNotSourceLang(lang, "English")) {
+      if (langName && isNotSourceLang(lang, "ENGLISH")) {
         return `${
-          document.querySelector("#result_box").innerText
-        }\n\nDetected language: ${langName}`;
+          document.querySelector("span.tlid-translation").innerText
+        }\n\nDetected language: ${langName.charAt(0) + langName.slice(1).toLowerCase()}`;
       }
       return null;
     });
@@ -83,8 +86,7 @@ const textToUrl = (text, lang = "en") =>
       .catch(e => logger.error(e))
       .then(translation => {
         if (translation) {
-          ctx.answerInlineQuery([
-            {
+          ctx.answerInlineQuery([{
               type: "article",
               id: 0,
               title: "Translation",
