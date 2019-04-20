98ae65da68751ec581596d8f581a0c1212facee0
David Roizenman <david@hmnd.io>
2019-04-20 00:11:01 -0400

    Add airbnb eslint

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-require("dotenv").config();
-const Telegraf = require("telegraf");
-const Extra = require("telegraf/extra");
-const puppeteer = require("puppeteer");
-const winston = require("winston");
-const titleCase = require("title-case");
+require('dotenv').config();
+const Telegraf = require('telegraf');
+const Extra = require('telegraf/extra');
+const puppeteer = require('puppeteer');
+const winston = require('winston');
+const titleCase = require('title-case');
 
-const tokens = process.env.BOT_TOKEN.split(",");
+const tokens = process.env.BOT_TOKEN.split(',');
 
 const logger = winston.createLogger({
-  level: "info",
+  level: 'info',
   format: winston.format.json(),
   transports: [
     new winston.transports.File({
-      filename: "error.log",
-      level: "error"
+      filename: 'error.log',
+      level: 'error'
     }),
     new winston.transports.File({
-      filename: "debug.log",
-      level: "debug"
+      filename: 'debug.log',
+      level: 'debug'
     })
   ]
 });
 
-const textToUrl = (text, lang = "en") =>
-  `https://translate.google.com/#view=home&op=translate&sl=auto&tl=${lang}&text=${encodeURIComponent(text)}`;
+const textToUrl = (text, lang = 'en') =>
+  `https://translate.google.com/#view=home&op=translate&sl=auto&tl=${lang}&text=${encodeURIComponent(
+    text
+  )}`;
 
 for (const token of tokens) {
   const bot = new Telegraf(token);
 
   (async () => {
     const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
       headless: true
     });
     const translateText = async inText => {
@@ -40,30 +42,36 @@ for (const token of tokens) {
       const page = await browser.newPage();
       await page
         .goto(textToUrl(inText), {
-          waitUntil: "networkidle0",
+          waitUntil: 'networkidle0',
           timeout: 0
         })
         .catch(e => logger.error(e).then(translateText(inText)));
       await page.waitForFunction(
         () =>
-        document.querySelector("span.tlid-translation") !== null &&
-        document.querySelector("span.tlid-translation").innerText.indexOf("......") === -1
+          document.querySelector('span.tlid-translation') !== null &&
+          document
+            .querySelector('span.tlid-translation')
+            .innerText.indexOf('......') === -1
       );
       const translation = await page.evaluate(() => {
         const isNotSourceLang = (sourceLang, destLang) =>
           !sourceLang.includes(destLang);
-        const extractLang = lang => lang.slice(0, lang.indexOf(" -")) || null;
-        const lang = document.querySelector("div.sl-sugg-button-container > div:first-child")
-          .innerText;
-        if (lang === "DETECT LANGUAGE") {
+        const extractLang = lang => lang.slice(0, lang.indexOf(' -')) || null;
+        const lang = document.querySelector(
+          'div.sl-sugg-button-container > div:first-child'
+        ).innerText;
+        if (lang === 'DETECT LANGUAGE') {
           return null;
         }
         const langName = extractLang(lang);
-        if (langName && isNotSourceLang(lang, "ENGLISH")) {
+        if (langName && isNotSourceLang(lang, 'ENGLISH')) {
           return {
             from: lang,
-            to: "ENGLISH",
-            text: `${document.querySelector("span.tlid-translation").innerText}\n\nDetected language: ${langName.charAt(0) + langName.slice(1).toLowerCase()}`
+            to: 'ENGLISH',
+            text: `${
+              document.querySelector('span.tlid-translation').innerText
+            }\n\nDetected language: ${langName.charAt(0) +
+              langName.slice(1).toLowerCase()}`
           };
         }
         return null;
@@ -72,7 +80,7 @@ for (const token of tokens) {
       return translation;
     };
 
-    bot.on("text", ctx => {
+    bot.on('text', ctx => {
       translateText(ctx.message.text)
         .then(translation => {
           if (translation) {
@@ -87,15 +95,18 @@ for (const token of tokens) {
         .catch(e => logger.error(e));
     });
 
-    bot.on("inline_query", ctx => {
+    bot.on('inline_query', ctx => {
       translateText(ctx.inlineQuery.query)
         .catch(e => logger.error(e))
         .then(translation => {
           if (translation) {
-            ctx.answerInlineQuery([{
-                type: "article",
+            ctx.answerInlineQuery([
+              {
+                type: 'article',
                 id: 0,
-                title: `Translation (${titleCase(translation.from)} => ${titleCase(translation.to)})`,
+                title: `Translation (${titleCase(
+                  translation.from
+                )} => ${titleCase(translation.to)})`,
                 description: translation.text,
                 input_message_content: {
                   message_text: translation.text,
@@ -103,22 +114,22 @@ for (const token of tokens) {
                 }
               },
               {
-                type: "article",
+                type: 'article',
                 id: 1,
-                title: "Translation - with translated text",
+                title: 'Translation - with translated text',
                 description: `Sends translated text, translation, and detected language.`,
                 input_message_content: {
                   message_text: `*Translated*: ${
-                  ctx.inlineQuery.query
-                }\n*Translation*: ${translation.text}`,
-                  parse_mode: "Markdown"
+                    ctx.inlineQuery.query
+                  }\n*Translation*: ${translation.text}`,
+                  parse_mode: 'Markdown'
                 }
               }
             ]);
           } else {
             ctx.answerInlineQuery([], {
-              switch_pm_text: "No translation found for inputted text.",
-              switch_pm_parameter: "no_trans_inline"
+              switch_pm_text: 'No translation found for inputted text.',
+              switch_pm_parameter: 'no_trans_inline'
             });
           }
         })
