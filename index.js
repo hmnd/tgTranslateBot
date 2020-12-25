4d067826d9539038346bdc83137e8513f2153f75
David Roizenman <david@hmnd.io>
2020-12-25 00:34:44 -0800

    Move to using Google Translate API directly

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
 const puppeteer = require('puppeteer');
 const winston = require('winston');
-const titleCase = require('title-case');
+const { titleCase } = require('title-case');
+const translate = require('@vitalets/google-translate-api');
+const languages = require('@vitalets/google-translate-api/languages');
 
 const tokens = (process.env.BOT_TOKEN || []).split(',');
 
@@ -22,110 +24,74 @@ const logger = winston.createLogger({
   ],
 });
 
-const textToUrl = (text, lang = 'en') => `https://translate.google.com/#view=home&op=translate&sl=auto&tl=${lang}&text=${encodeURIComponent(
-  text,
-)}`;
-
 tokens.forEach((token) => {
   const bot = new Telegraf(token);
 
-  (async () => {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      headless: true,
-    });
-    const translateText = async (inText) => {
-      if (!inText.trim().length) {
-        return null;
+  bot.on('text', async (ctx) => {
+    try {
+      const translation = await translate(ctx.message.text, { to: 'en' });
+
+      if (translation) {
+        return ctx.replyWithMarkdown(
+          translation.text,
+          Extra.inReplyTo(ctx.message.message_id)
+        );
       }
-      const page = await browser.newPage();
-      await page
-        .goto(textToUrl(inText), {
-          waitUntil: 'networkidle0',
-          timeout: 0,
-        })
-        .catch(e => logger.error(e).then(translateText(inText)));
-      await page.waitForFunction(
-        () => document.querySelector('span.tlid-translation') !== null
-          && document.querySelector('span.tlid-translation').innerText.indexOf('......') === -1,
-      );
-      const translation = await page.evaluate(() => {
-        const isNotSourceLang = (sourceLang, destLang) => !sourceLang.includes(destLang);
-        const extractLang = lang => lang.slice(0, lang.indexOf(' -')) || null;
-        const lang = document.querySelector('div.sl-sugg-button-container > div:first-child')
-          .innerText;
-        if (lang === 'DETECT LANGUAGE') {
-          return null;
-        }
-        const langName = extractLang(lang);
-        if (langName && isNotSourceLang(lang, 'ENGLISH')) {
-          return {
-            from: lang,
-            to: 'ENGLISH',
-            text: `${
-              document.querySelector('span.tlid-translation').innerText
-            }\n\nDetected language: ${langName.charAt(0) + langName.slice(1).toLowerCase()}`,
-          };
-        }
-        return null;
+    } catch (e) {
+      logger.error(e);
+    }
+  });
+
+  bot.on('inline_query', async (ctx) => {
+    try {
+      const { query } = ctx.inlineQuery;
+      if (query && query.trim().length === 0) {
+        return ctx.answerInlineQuery([], {
+          switch_pm_text: 'Enter some text to translate',
+          switch_pm_parameter: 'empty_inline',
+        });
+      }
+      const translateTo = 'en';
+      const translation = await translate(ctx.inlineQuery.query, {
+        to: translateTo,
       });
-      await page.close();
-      return translation;
-    };
+      console.log(languages[translateTo]);
+      if (translation) {
+        return ctx.answerInlineQuery([
+          {
+            type: 'article',
+            id: 0,
+            title: `Translation (${titleCase(
+              languages[translation.from.language.iso]
+            )} => ${titleCase(languages[translateTo])})`,
+            description: translation.text,
+            input_message_content: {
+              message_text: translation.text,
+              parse_mode: Extra.markdown,
+            },
+          },
+          {
+            type: 'article',
+            id: 1,
+            title: 'Translation - with translated text',
+            description:
+              'Sends translated text, translation, and detected language.',
+            input_message_content: {
+              message_text: `*Original*: ${ctx.inlineQuery.query}\n*Translation*: ${translation.text}`,
+              parse_mode: 'Markdown',
+            },
+          },
+        ]);
+      }
+      return ctx.answerInlineQuery([], {
+        switch_pm_text: 'No translation found for inputted text.',
+        switch_pm_parameter: 'no_trans_inline',
+      });
+    } catch (e) {
+      console.log(e);
+      logger.error(e);
+    }
+  });
 
-    bot.on('text', (ctx) => {
-      translateText(ctx.message.text)
-        .then((translation) => {
-          if (translation) {
-            ctx
-              .replyWithMarkdown(translation.text, Extra.inReplyTo(ctx.message.message_id))
-              .catch(e => logger.error(e));
-          }
-        })
-        .catch(e => logger.error(e));
-    });
-
-    bot.on('inline_query', (ctx) => {
-      translateText(ctx.inlineQuery.query)
-        .catch(e => logger.error(e))
-        .then((translation) => {
-          if (translation) {
-            ctx.answerInlineQuery([
-              {
-                type: 'article',
-                id: 0,
-                title: `Translation (${titleCase(translation.from)} => ${titleCase(
-                  translation.to,
-                )})`,
-                description: translation.text,
-                input_message_content: {
-                  message_text: translation.text,
-                  parse_mode: Extra.markdown,
-                },
-              },
-              {
-                type: 'article',
-                id: 1,
-                title: 'Translation - with translated text',
-                description: 'Sends translated text, translation, and detected language.',
-                input_message_content: {
-                  message_text: `*Original*: ${ctx.inlineQuery.query}\n*Translation*: ${
-                    translation.text
-                  }`,
-                  parse_mode: 'Markdown',
-                },
-              },
-            ]);
-          } else {
-            ctx.answerInlineQuery([], {
-              switch_pm_text: 'No translation found for inputted text.',
-              switch_pm_parameter: 'no_trans_inline',
-            });
-          }
-        })
-        .catch(e => logger.error(e));
-    });
-
-    bot.startPolling();
-  })();
+  bot.startPolling();
 });
